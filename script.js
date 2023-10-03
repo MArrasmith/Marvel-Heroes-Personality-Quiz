851afbd4331290cffc8964b46b0399e333df36eb
neon <dogmanyo@gmail.com>
2023-10-02 20:30:49 -0400

    add javascript code

--- script.js
+++ script.js
@@ -4,10 +4,40 @@ const timestamp = new Date().getTime();
 const dataHash = timestamp + privateKey + publicKey;
 const hash = CryptoJS.MD5(dataHash).toString();// Create a hash based on timestamp and private key (you'll need a hashing library for this)
 
+      const questions = document.querySelectorAll('.question');
+        const nextBtn = document.getElementById('nextBtn');
+        let currentQuestion = 0;
+
+        function showQuestion(questionIndex) {
+            questions.forEach((question, index) => {
+                if (index === questionIndex) {
+                    question.style.display = 'block';
+                } else {
+                    question.style.display = 'none';
+                }
+            });
+        }
+
+        nextBtn.addEventListener('click', () => {
+            currentQuestion++;
+            if (currentQuestion < questions.length) {
+                showQuestion(currentQuestion);
+            } else {
+                // Quiz completed, you can handle this case here
+                alert('Quiz completed!');
+            }
+        });
+
+        // Show the first question initially
+        showQuestion(currentQuestion);
+
+
 const apiUrl = `https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
 
-  // Function to fetch and display Giphy data
+
   
+function getHeroGif(){
+    // Function to fetch and display Giphy data
     const apiKey = "JH9332CExLkO5kD2VRLsKceFMMT5HUCO";
     const giphyEndpoint = "https://api.giphy.com/v1/gifs/search?api_key=JH9332CExLkO5kD2VRLsKceFMMT5HUCO&?q=marvel";
 
@@ -21,42 +51,20 @@ const apiUrl = `https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&
       .catch((error) => {
         console.error("Error fetching Giphy data:", error);
       });
-  
+}
 
- 
-
-
-var startButton = document.querySelector("#helpUs");
-var isComplete;
-
-
-
-
-fetch(apiUrl)
+function getHeroInfo(heroname){
+  fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
     // Process and display data on your website
     console.log(data);
+    
   })
   .catch(error => {
     console.error('Error fetching data:', error);
   });
+}
 
-  function startGame() {
-    isComplete = false;
-    // Prevents start button from being clicked when round is in progress
-    startButton.disabled = true;
-    renderQuestion(); 
-  }
 
-  function renderQuestion() {
-    
-    
   
-      questionCard.textContent = chosenQuestion;
-  
-  }
-
-  startButton.addEventListener("click", startGame);
-
- 
